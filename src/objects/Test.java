cf3b7b5975f5ed18843f1c1f5dc5146188abf304
emr3k0c <emr3k0c@yahoo.com>
2024-05-22 03:07:33 +0300

    test object added

--- src/objects/Test.java
+++ src/objects/Test.java
@@ -1,5 +1,60 @@
 package objects;
 
 public class Test {
+	private int id;
+	private int patientID;
+	private int appointmentID;
+	private String description;
+	private double billing;
+	
+	public Test(int id, int patientID, int appointmentID, String description, double billing) {
+		this.id = id;
+		this.patientID = patientID;
+		this.appointmentID = appointmentID;
+		this.description = description;
+		this.billing = billing;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPatientID() {
+		return patientID;
+	}
+
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+
+	public int getAppointmentID() {
+		return appointmentID;
+	}
+
+	public void setAppointmentID(int appointmentID) {
+		this.appointmentID = appointmentID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getBilling() {
+		return billing;
+	}
+
+	public void setBilling(double billing) {
+		this.billing = billing;
+	}
+	
+	
+	
 }
